e105e6575f7188659a45da9bed0e8deadf3930f8
Matias Mesa <matias.mesaj@gmail.com>
2021-12-31 03:10:57 -0300

    popover and alert ready

---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+});
+
+const AlertaDatos1 = document.getElementById('card-alert1');
+
+AlertaDatos1.addEventListener('click', function(){
+    Swal.fire({
+        title: '¡Ven y conoceme!',
+        text: 'pasa por la sección de contacto y te entregaremos los mejores datos para el viaje',
+        imageUrl: 'assets/img/card1.jpg',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'card1',
+        confirmButtonColor: '#17a3b8',
+      })   
+    
+})
+
+const AlertaDatos2 = document.getElementById('card-alert2');
+
+AlertaDatos2.addEventListener('click', function(){
+    Swal.fire({
+        title: '¡Ven y conoceme!',
+        text: 'pasa por la sección de contacto y te entregaremos los mejores datos para el viaje',
+        imageUrl: 'assets/img/card2.jpg',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'card3',
+        confirmButtonColor: '#17a3b8',
+      })   
+    
+})
+
+const AlertaDatos3 = document.getElementById('card-alert3');
+
+AlertaDatos3.addEventListener('click', function(){
+    Swal.fire({
+        title: '¡Ven y conoceme!',
+        text: 'pasa por la sección de contacto y te entregaremos los mejores datos para el viaje',
+        imageUrl: 'assets/img/card3.jpg',
+        imageWidth: 400,
+        imageHeight: 200,
+        imageAlt: 'card3',
+        confirmButtonColor: '#17a3b8',
+      })   
+    
+})
+
+
+  
